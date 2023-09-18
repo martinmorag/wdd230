472e02bdb625b56d9e5bd829c1c5c342816f7d1f
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-09-18 15:25:08 -0300

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -26,7 +26,36 @@ window.weatherWidgetConfig =  window.weatherWidgetConfig || [];
        selector:".weatherWidget",
        apiKey:"6HFDLKEXMR3DR8S9KUN2CYDHC", //Sign up for your personal key
        location:"Buenos Aires, AR", //Enter an address
-       unitGroup:"metric", //"us" or "metric"
+       unitGroup:"metric", //"us" or "metric"function formatDate(date) {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
+    return date.toLocaleDateString(undefined, options);
+}
+
+// Get the last modified date of the current document
+const lastModified = new Date(document.lastModified);
+
+// Format the last modified date
+const formattedLastModified = formatDate(lastModified);
+
+// Update the content of the "lastModified" element with the formatted date
+const lastModifiedElement = document.getElementById('lastModified');
+lastModifiedElement.textContent = formattedLastModified;
+
+
+
+const currentYear = new Date().getFullYear();
+
+const currentYearElement = document.getElementById('currentYear');
+currentYearElement.textContent = currentYear;
+
+/* Hamburger */
+
+const menuToggle = document.getElementById('menu-toggle');
+const navUL = document.querySelector('.navigation ul');
+
+menuToggle.addEventListener('click', () => {
+  navUL.classList.toggle('active');
+});
        forecastDays:5, //how many days forecast to show
        title:"Buenos Aires, AR", //optional title to show in the 
        showTitle:true, 
