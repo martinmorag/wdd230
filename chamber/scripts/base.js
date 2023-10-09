30bdf389d76aa0fa25df525110cdae47185a4658
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-10-09 15:03:21 -0300

    Update base.js

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -31,16 +31,48 @@ hamButton.addEventListener('click', () => {
 
 const darkModeButton = document.querySelector('#dark')
 const content = document.querySelector('main')
+const title = document.querySelector('.title')
+
+/* boxes selection */
+const events = document.querySelector('.event')
+const weather = document.querySelector('.weather')
+const first_review = document.querySelector('.first_review')
+const second_review = document.querySelector('.second_review')
+const events_link = document.querySelector('.event a')
+const freview_link = document.querySelector('.first_review a')
+const sreview_link = document.querySelector('.second_review a')
 
 let isDarkMode = localStorage.getItem('darkMode') === 'true';
 
 function setMode(isDark) {
     if (isDark) {
         content.style.backgroundColor = '#0B132B';
-        content.style.color = 'white';
+        title.style.color = 'white';
+        events.style.color = 'black';
+        events_link.style.color = 'black';
+        events.style.backgroundColor = "#f4faf4";
+        weather.style.color = 'black';
+        weather.style.backgroundColor = "#f4faf4";
+        first_review.style.color = 'black';
+        first_review.style.backgroundColor = "#f4faf4";
+        second_review.style.color = 'black';
+        second_review.style.backgroundColor = "#f4faf4";
+        freview_link.style.color = 'black';
+        sreview_link.style.color = 'black';
     } else {
         content.style.backgroundColor = 'white';
-        content.style.color = '#0B132B';
+        title.style.color = '#0B132B';
+        events.style.color = '#f4faf4';
+        events_link.style.color = 'white';
+        events.style.backgroundColor = "#0B132B";
+        weather.style.color = '#f4faf4';
+        weather.style.backgroundColor = "#0B132B";
+        first_review.style.color = '#f4faf4';
+        first_review.style.backgroundColor = "#0B132B";
+        second_review.style.color = '#f4faf4';
+        second_review.style.backgroundColor = "#0B132B";
+        freview_link.style.color = 'white';
+        sreview_link.style.color = 'white';
     }
 }
 
