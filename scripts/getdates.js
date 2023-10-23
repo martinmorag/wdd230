0eced23d9324d714b71d5753a95aaf05cf3c282f
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-10-23 14:07:27 -0300

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -28,3 +28,24 @@ const navUL = document.querySelector('.navigation ul');
 menuToggle.addEventListener('click', () => {
   navUL.classList.toggle('active');
 });
+
+
+/* the number of visits */
+
+// Check if the 'pageVisits' key exists in localStorage
+if (localStorage.getItem('pageVisits') === null) {
+  // If not, initialize it with a value of 1
+  localStorage.setItem('pageVisits', '1');
+} else {
+  // If it exists, increment the value by 1
+  let currentVisits = parseInt(localStorage.getItem('pageVisits'));
+  currentVisits++;
+  localStorage.setItem('pageVisits', currentVisits.toString());
+}
+
+// Get the updated total number of visits from localStorage
+const totalVisits = localStorage.getItem('pageVisits');
+
+// Display the total number of visits on your webpage
+const visitsElement = document.getElementById('visits'); // Assuming you have an HTML element with id 'visits' to display the count
+visitsElement.textContent = `${totalVisits} times.`;
