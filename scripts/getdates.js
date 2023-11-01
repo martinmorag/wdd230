898bf2e72db5a448fbbd9a384f8ab03061203d79
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-11-01 08:22:12 +0400

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -49,3 +49,39 @@ const totalVisits = localStorage.getItem('pageVisits');
 // Display the total number of visits on your webpage
 const visitsElement = document.getElementById('visits'); // Assuming you have an HTML element with id 'visits' to display the count
 visitsElement.textContent = `${totalVisits} times.`;
+
+
+
+
+/* FORM re-enter password js */
+const ps1 = document.querySelector("#password");
+const ps2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+ps2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+	if (ps1.value !== ps2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		ps2.style.backgroundColor = "#fff0f3";
+		ps2.value = "";
+		ps2.focus();
+	} else {
+		message.style.display = "none";
+		ps2.style.backgroundColor = "#fff";
+		ps2.style.color = "#000";
+	}
+}
+
+/* RATING OPTION IN FORM */
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("rating");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
