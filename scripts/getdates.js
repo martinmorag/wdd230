d7c7e7831a575f2c31563726dc103bd33f1c695a
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-11-13 20:34:03 +0400

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,12 +3,9 @@ const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("rating");
 
 // RANGE event listener
-range.addEventListener('change', displayRatingValue);
-range.addEventListener('input', displayRatingValue);
-
-function displayRatingValue() {
-    rangevalue.innerHTML = range.value;
-}
+function updateRating(value) {
+	document.getElementById('value').innerHTML = value;
+  }
 
 
 /* FORM re-enter password js */
