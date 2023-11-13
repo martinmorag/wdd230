f8a96f98dd3cf6cc233de7aa14b75206f7576348
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-11-13 20:52:41 +0400

    Update form.js

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,35 @@
 function updateRating(value) {
       document.getElementById('value').innerHTML = value;
 }
+
+
+/* FORM re-enter password js */
+const ps1 = document.querySelector("#password");
+const ps2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+ps2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+	if (ps1.value !== ps2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		ps2.style.backgroundColor = "#fff0f3";
+		ps2.value = "";
+		ps2.focus();
+	} else {
+		message.style.display = "none";
+		ps2.style.backgroundColor = "#fff";
+		ps2.style.color = "#000";
+	}
+}
+
+
+/* Hamburger */
+
+const menuToggle = document.getElementById('menu-toggle');
+const navUL = document.querySelector('.navigation ul');
+
+menuToggle.addEventListener('click', () => {
+  navUL.classList.toggle('active');
+});
