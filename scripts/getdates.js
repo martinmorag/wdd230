8dba62e69ceefe3974a26489e0b9de83e7925337
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-11-13 20:44:58 +0400

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,6 +5,29 @@ function updateRating(value) {
 
 
 /* FORM re-enter password js */
+const ps1 = document.querySelector("#password");
+const ps2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+ps2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+	if (ps1.value !== ps2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		ps2.style.backgroundColor = "#fff0f3";
+		ps2.value = "";
+		ps2.focus();
+	} else {
+		message.style.display = "none";
+		ps2.style.backgroundColor = "#fff";
+		ps2.style.color = "#000";
+	}
+}
+
+
+
+/* FORM re-enter password js 
 
 function validate_password() {
  
@@ -32,7 +55,7 @@ function validate_password() {
             } else {
                 alert("Please fill all the fields");
             }
-        }
+        }   */
 
 
 
