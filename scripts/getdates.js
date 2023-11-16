395fec8c0970b7e8e14d73abb1d9c6e1a0eefa05
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-11-16 17:08:57 +0400

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,58 @@
+function formatDate(date) {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
+    return date.toLocaleDateString(undefined, options);
+}
+
+// Get the last modified date of the current document
+const lastModified = new Date(document.lastModified);
+
+// Format the last modified date
+const formattedLastModified = formatDate(lastModified);
+
+// Update the content of the "lastModified" element with the formatted date
+const lastModifiedElement = document.getElementById('lastModified');
+lastModifiedElement.textContent = formattedLastModified;
+
+
+
+const currentYear = new Date().getFullYear();
+
+const currentYearElement = document.getElementById('currentYear');
+currentYearElement.textContent = currentYear;
+
+/* Hamburger */
+
+const menuToggle = document.getElementById('menu-toggle');
+const navUL = document.querySelector('.navigation ul');
+
+menuToggle.addEventListener('click', () => {
+  navUL.classList.toggle('active');
+});
+
+
+/* the number of visits */
+
+// Check if the 'pageVisits' key exists in localStorage
+if (localStorage.getItem('pageVisits') === null) {
+  // If not, initialize it with a value of 1
+  localStorage.setItem('pageVisits', '1');
+} else {
+  // If it exists, increment the value by 1
+  let currentVisits = parseInt(localStorage.getItem('pageVisits'));
+  currentVisits++;
+  localStorage.setItem('pageVisits', currentVisits.toString());
+}
+
+// Get the updated total number of visits from localStorage
+const totalVisits = localStorage.getItem('pageVisits');
+
+// Display the total number of visits on your webpage
+const visitsElement = document.getElementById('visits'); // Assuming you have an HTML element with id 'visits' to display the count
+visitsElement.textContent = `${totalVisits} times.`;
+
+
+
+
 /* WEATHER */
 
 const currentTemp = document.querySelector('#current-temp');
@@ -32,33 +87,8 @@ function displayResults(data) {
 }
 
 
-/*
 
-function formatDate(date) {
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
-    return date.toLocaleDateString(undefined, options);
-}
-
-// Get the last modified date of the current document
-const lastModified = new Date(document.lastModified);
-
-// Format the last modified date
-const formattedLastModified = formatDate(lastModified);
-
-// Update the content of the "lastModified" element with the formatted date
-const lastModifiedElement = document.getElementById('lastModified');
-lastModifiedElement.textContent = formattedLastModified;
-
-
-
-const currentYear = new Date().getFullYear();
-
-const currentYearElement = document.getElementById('currentYear');
-currentYearElement.textContent = currentYear;
-
-
-*/
-/* LINKS 
+/* LINKS */
 
 const baseUrl = 'https://martinmorag.github.io/wdd230/';
 const linksUrl = 'https://martinmorag.github.io/wdd230/data/links.json'
@@ -88,39 +118,3 @@ function displayLinks(data) {
    weeks[6].setAttribute('href', data.weeks[3].links[0].url);
    weeks[6].textContent = ` ${data.weeks[3].links[0].title} `;
 }
-
-*/
-
-/* Hamburger 
-
-const menuToggle = document.getElementById('menu-toggle');
-const navUL = document.querySelector('.navigation ul');
-
-menuToggle.addEventListener('click', () => {
-  navUL.classList.toggle('active');
-});
-
-
-
-*/
-/* the number of visits 
-
-// Check if the 'pageVisits' key exists in localStorage
-if (localStorage.getItem('pageVisits') === null) {
-  // If not, initialize it with a value of 1
-  localStorage.setItem('pageVisits', '1');
-} else {
-  // If it exists, increment the value by 1
-  let currentVisits = parseInt(localStorage.getItem('pageVisits'));
-  currentVisits++;
-  localStorage.setItem('pageVisits', currentVisits.toString());
-}
-
-// Get the updated total number of visits from localStorage
-const totalVisits = localStorage.getItem('pageVisits');
-
-// Display the total number of visits on your webpage
-const visitsElement = document.getElementById('visits'); // Assuming you have an HTML element with id 'visits' to display the count
-visitsElement.textContent = `${totalVisits} times.`;
-
-*/
