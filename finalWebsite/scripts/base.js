203e9bfef5c0b120eac0abdc3c0cb897ce4f905e
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-12-12 14:41:39 -0300

    Update base.js

--- finalWebsite/scripts/base.js
+++ finalWebsite/scripts/base.js
@@ -189,3 +189,13 @@ langBtn.addEventListener('click', function() {
   
   changeLang(whichLanguage);
 });
+
+/* HAMBURGER MENU */
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+navigation.classList.toggle('open');
+hamButton.classList.toggle('open');
+});
