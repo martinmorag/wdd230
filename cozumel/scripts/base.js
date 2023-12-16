98baa3449c98002d078c9f9fb3d493549a2da017
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-12-15 23:13:58 -0300

    Add files via upload

---
+++ cozumel/scripts/base.js
@@ -0,0 +1,102 @@
+/* PRICING */
+const offers = document.querySelectorAll('.offer');
+const url = "https://martinmorag.github.io/wdd230/Cozumel/data/pricing.json";
+
+async function apiFetchOffers() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayOffers(data);
+        } else {
+            throw Error (await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetchOffers();
+
+function displayOffers(data) {
+    let fType = data.rental[0].scooters[2].type;
+    let fCc = data.rental[0].scooters[2].cc;
+    let fFullDay = data.rental[0].scooters[2].reservation[0].fullDay;
+    offers[0].innerHTML = `for a Full Day and for the price of only $${fFullDay} you can get the ${fCc}CC ${fType}!`;
+
+    let sType = data.rental[1].ATVs[0].type;
+    let sHalfDay = data.rental[1].ATVs[0].reservation[0].halfDayPrice;
+    offers[1].innerHTML = `If you make a reservation for half day use of the ATV, you can get the ${sType} for only $${sHalfDay}!`;
+
+    let tType = data.rental[2].Jeeps[0].type;
+    let tCapacity = data.rental[2].Jeeps[0].maxPersons;
+    let tFullDay = data.rental[2].Jeeps[0].walkIn[0].fullDay;
+    offers[2].innerHTML = `And if you are in a hurry with a lot of people, you can rent a Jeep, that can take up to ${tCapacity} people, this is the ${tType} and you can get it for $${tFullDay} and enjoy it all day long!`;
+};
+
+
+/* WEATHER */
+
+const currentTemp = document.querySelector('#current-temp');
+const currentHumidity = document.querySelector('#current-humidity');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('.caption');
+const forecast = document.querySelector('#forecast');
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=20.51&lon=-86.94&appid=71af0db7deda29b809c083c77221039b&units=metric';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(weatherUrl);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+
+            // Fetch and display 3-day forecast
+            const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=20.51&lon=-86.94&appid=71af0db7deda29b809c083c77221039b&units=metric`;
+            const forecastResponse = await fetch(forecastUrl);
+            if (forecastResponse.ok) {
+                const forecastData = await forecastResponse.json();
+                displayForecast(forecastData);
+            } else {
+                throw Error(await forecastResponse.text());
+            }
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    const roundedTemp = Math.round(data.main.temp);
+    const humidity = data.main.humidity;
+    currentTemp.innerHTML = `${roundedTemp}&deg;C`;
+    currentHumidity.innerHTML = `Humidity: ${humidity}%`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.src = iconsrc || 'path/to/placeholder-image.jpg';
+    weatherIcon.alt = 'Weather Icon' || 'Default Alt Text';
+    captionDesc.textContent = desc;
+}
+
+function displayForecast(forecastData) {
+    const nextDayForecastTemp = Math.round(forecastData.list[0].main.temp);
+    const nextDayForecastMain = forecastData.list[0].weather[0].main;
+    const nextDayForecastDesc = forecastData.list[0].weather[0].description;
+    const nextDayForecastIcon = forecastData.list[0].weather[0].icon;
+
+    forecast.innerHTML = `The next's day temperature at 15:00 will be ${nextDayForecastTemp}&deg;C`;
+}
+
+/* HAMBURGUER BUTTON */
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+navigation.classList.toggle('open');
+hamButton.classList.toggle('open');
+});
---
+++ cozumel/scripts/rentals.js
@@ -0,0 +1,78 @@
+/* PRICING */
+const type = document.querySelectorAll('.type');
+const persons = document.querySelectorAll('.persons');
+const fHalfDay = document.querySelectorAll('.f_half_day');
+const fFullDay = document.querySelectorAll('.f_full_day');
+const sHalfDay = document.querySelectorAll('.s_half_day');
+const sFullDay = document.querySelectorAll('.s_full_day');
+const url = "https://martinmorag.github.io/wdd230/Cozumel/data/pricing.json";
+
+async function apiFetchOffers() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayOffers(data);
+        } else {
+            throw Error (await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetchOffers();
+
+function displayOffers(data) {
+    type[0].innerHTML = data.rental[0].scooters[0].type;
+    persons[0].innerHTML = data.rental[0].scooters[0].maxPersons;
+    fHalfDay[0].innerHTML = data.rental[0].scooters[0].reservation[0].halfDayPrice;
+    fFullDay[0].innerHTML = data.rental[0].scooters[0].reservation[0].fullDay;
+    sHalfDay[0].innerHTML = data.rental[0].scooters[0].walkIn[0].halfDayPrice;
+    sFullDay[0].innerHTML = data.rental[0].scooters[0].walkIn[0].fullDay;
+
+    type[1].innerHTML = data.rental[0].scooters[1].type;
+    persons[1].innerHTML = data.rental[0].scooters[1].maxPersons;
+    fHalfDay[1].innerHTML = data.rental[0].scooters[1].reservation[0].halfDayPrice;
+    fFullDay[1].innerHTML = data.rental[0].scooters[1].reservation[0].fullDay;
+    sHalfDay[1].innerHTML = data.rental[0].scooters[1].walkIn[0].halfDayPrice;
+    sFullDay[1].innerHTML = data.rental[0].scooters[1].walkIn[0].fullDay;
+
+    type[2].innerHTML = data.rental[0].scooters[2].type;
+    persons[2].innerHTML = data.rental[0].scooters[2].maxPersons;
+    fHalfDay[2].innerHTML = data.rental[0].scooters[2].reservation[0].halfDayPrice;
+    fFullDay[2].innerHTML = data.rental[0].scooters[2].reservation[0].fullDay;
+    sHalfDay[2].innerHTML = data.rental[0].scooters[2].walkIn[0].halfDayPrice;
+    sFullDay[2].innerHTML = data.rental[0].scooters[2].walkIn[0].fullDay;
+
+    type[3].innerHTML = data.rental[1].ATVs[0].type;
+    persons[3].innerHTML = data.rental[1].ATVs[0].maxPersons;
+    fHalfDay[3].innerHTML = data.rental[1].ATVs[0].reservation[0].halfDayPrice;
+    fFullDay[3].innerHTML = data.rental[1].ATVs[0].reservation[0].fullDay;
+    sHalfDay[3].innerHTML = data.rental[1].ATVs[0].walkIn[0].halfDayPrice;
+    sFullDay[3].innerHTML = data.rental[1].ATVs[0].walkIn[0].fullDay;
+
+    type[4].innerHTML = data.rental[2].Jeeps[0].type;
+    persons[4].innerHTML = data.rental[2].Jeeps[0].maxPersons;
+    fHalfDay[4].innerHTML = data.rental[2].Jeeps[0].reservation[0].halfDayPrice;
+    fFullDay[4].innerHTML = data.rental[2].Jeeps[0].reservation[0].fullDay;
+    sHalfDay[4].innerHTML = data.rental[2].Jeeps[0].walkIn[0].halfDayPrice;
+    sFullDay[4].innerHTML = data.rental[2].Jeeps[0].walkIn[0].fullDay;
+
+    type[5].innerHTML = data.rental[2].Jeeps[1].type;
+    persons[5].innerHTML = data.rental[2].Jeeps[1].maxPersons;
+    fHalfDay[5].innerHTML = data.rental[2].Jeeps[1].reservation[0].halfDayPrice;
+    fFullDay[5].innerHTML = data.rental[2].Jeeps[1].reservation[0].fullDay;
+    sHalfDay[5].innerHTML = data.rental[2].Jeeps[1].walkIn[0].halfDayPrice;
+    sFullDay[5].innerHTML = data.rental[2].Jeeps[1].walkIn[0].fullDay;
+}
+
+/* HAMBURGUER BUTTON */
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+navigation.classList.toggle('open');
+hamButton.classList.toggle('open');
+});
